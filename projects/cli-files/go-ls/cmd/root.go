b7f8d7710cfb6a9c66a36aa4a1b5fad69577e57e
Cirlorm <akotosel6@gmail.com>
2024-11-20 17:51:42 +0000

    mod: refactored the code to be more simpler

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -6,28 +6,24 @@ import (
 )
 
 func Execute() error {
-	if len(os.Args[1]) > 0 {
-		files, err := os.ReadDir(os.Args[1])
-		if err != nil {
-			return fmt.Errorf("error reading files from dir")
-		}
-
-		for _, file := range files {
-			fmt.Println(file.Name())
-		}
-
-		return nil
-
-	} else {
-		files, err := os.ReadDir(".")
-		if err != nil {
-			return fmt.Errorf("error reading files from dir")
-		}
-
-		for _, file := range files {
-			fmt.Println(file.Name())
-		}
-
-		return nil
+	dir := "."
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
 	}
+
+	err := readDir(dir)
+	return err
+}
+
+func readDir(dir string) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("error reading files from dir")
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+
+	return nil
 }
